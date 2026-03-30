0b4dc2eeacdcb3ee85fcf161fe99c63d574b25a0
Sebastian Garzon <jsgarzonp@estud.usfq.edu.ec>
2026-03-30 17:10:01 -0500

    Create TallerChocolate.java
    taller chocolate

---
+++ TallerChocolate.java
@@ -0,0 +1,6 @@
+public class TallerChocolate implements Evento {
+    @Override
+    public void mostrarDetalle() {
+        System.out.println("Taller: Elaboración artesanal de chocolate");
+    }
+}
