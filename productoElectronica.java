7e10f54dec66dd28d34b917d229ac5f6daf7dcdc
Sebastian Garzon <jsgarzonp@estud.usfq.edu.ec>
2026-03-30 17:06:25 -0500

    Create productoElectronica.java
    producto electronica

---
+++ productoElectronica.java
@@ -0,0 +1,6 @@
+public class TallerMusicaElectronica implements Evento {
+    @Override
+    public void mostrarDetalle() {
+        System.out.println("Taller: Producción de beats y mezcla DJ");
+    }
+}
