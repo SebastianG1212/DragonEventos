7ea4cde46eeeea5f8ec984d2cd65fb40e628c8d6
Sebastian Garzon <jsgarzonp@estud.usfq.edu.ec>
2026-03-30 17:10:53 -0500

    Create ConciertoChocolate.java
    concierto chocolate

---
+++ ConciertoChocolate.java
@@ -0,0 +1,6 @@
+public class ConciertoChocolate implements Evento {
+    @Override
+    public void mostrarDetalle() {
+        System.out.println("Show principal: Degustación y experiencia sensorial del chocolate");
+    }
+}
