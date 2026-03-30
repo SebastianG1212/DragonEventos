37f2e5dc08f1162f46212bcf7b85a811efcefd38
Sebastian Garzon <jsgarzonp@estud.usfq.edu.ec>
2026-03-30 17:07:35 -0500

    Create ProductoConcierto.java
    Producto concierto

---
+++ ProductoConcierto.java
@@ -0,0 +1,6 @@
+public class ConciertoMusicaElectronica implements Evento {
+    @Override
+    public void mostrarDetalle() {
+        System.out.println("Concierto: Presentación principal de música electrónica");
+    }
+}
